c07c8e2b490afc3b398f26abb5d73167d2935c80
Pasha <54247708+DeathBarmaglot@users.noreply.github.com>
2021-02-26 16:18:58 +0200

    Update index_edgeActions.js

--- index_edgeActions.js
+++ index_edgeActions.js
@@ -1,15 +1,7 @@
-/***********************
-* Adobe Edge Animate Composition Actions
-*
-* Edit this file with caution, being careful to preserve 
-* function signatures and comments starting with 'Edge' to maintain the 
-* ability to interact with these actions from within Adobe Edge Animate
-*
-***********************/
+
 (function($, Edge, compId){
 var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonly used Edge classes
 
-   //Edge symbol: 'stage'
    (function(symbolName) {
       
       
@@ -18,9 +10,9 @@ var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonl
          sym.play(0);
 
       });
-      //Edge binding end
+      
 
    })("stage");
-   //Edge symbol end:'stage'
+   
 
 })(jQuery, AdobeEdge, "EDGE-2016667");
